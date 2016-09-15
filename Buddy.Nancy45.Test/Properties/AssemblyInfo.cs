89ecd171e3831033f35d824364d542b19122cb2f
Chris Hogan <hogancm@gmail.com>
2016-09-15 15:57:17 -0400

    Project Created

---
+++ Buddy.Nancy45.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("Buddy.Nancy45.Test")]
+[assembly: AssemblyDescription("Tests for the Buddy.Nancy assembly")]
+[assembly: AssemblyConfiguration("")]
---
+++ Buddy.Nancy45/Serialization/CustomJsonSerializer.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+
+namespace Buddy.Nancy.Serialization
+{
+    public class CustomJsonSerializer : JsonSerializer
+    {
+        public CustomJsonSerializer()
+        {
+            DateFormatHandling = Newtonsoft.Json.DateFormatHandling.IsoDateFormat;
+            DateTimeZoneHandling = Newtonsoft.Json.DateTimeZoneHandling.Utc;
+            MissingMemberHandling = Newtonsoft.Json.MissingMemberHandling.Ignore;
+        }
+    }
+}
