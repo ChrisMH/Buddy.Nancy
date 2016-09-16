325d5d57579f6ba6a6b73ba249a212323222d3fb
Chris Hogan <hogancm@gmail.com>
2016-09-16 17:52:02 -0400

    Added additional CustomJsonSerializer constructors

--- Buddy.Nancy45/Serialization/CustomJsonSerializer.cs
+++ Buddy.Nancy45/Serialization/CustomJsonSerializer.cs
@@ -1,11 +1,22 @@
-﻿using Newtonsoft.Json;
+﻿using System.Xml;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
 
 namespace Buddy.Nancy.Serialization
 {
     public class CustomJsonSerializer : JsonSerializer
     {
-        public CustomJsonSerializer(bool camelCase = true, bool indented = true)
+        public CustomJsonSerializer()
+            : this(true, true)
+        {
+        }
+
+        public CustomJsonSerializer(bool camelCase)
+            : this(camelCase, true)
+        {
+        }
+
+        public CustomJsonSerializer(bool camelCase, bool indented)
         {
             DateFormatHandling = Newtonsoft.Json.DateFormatHandling.IsoDateFormat;
             DateTimeZoneHandling = Newtonsoft.Json.DateTimeZoneHandling.Utc;
@@ -15,7 +26,7 @@ namespace Buddy.Nancy.Serialization
                 ContractResolver = new CamelCasePropertyNamesContractResolver();
 
             if (indented)
-                Formatting = Formatting.Indented;
+                Formatting = Newtonsoft.Json.Formatting.Indented;
         }
     }
 }
